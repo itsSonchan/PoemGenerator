00ad1e41d027b8cd6f074eb54a966e0b16d18107
TheSonchan <sonja.grund@icloud.com>
2025-03-23 14:49:26 +0100

    Changed the context of the AI prompt

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
+function displayPoem(response) {
+  new Typewriter("#poem-field", {
+    strings: response.data.answer,
+    autoStart: true,
+    cursor: "",
+    delay: 1,
+  });
+}
+
 function apiCall(topic) {
   let apiKey = "4d6d3a603f2o058afbtc1e8fa6515357";
-  let prompt = `Write a short poem about ${topic}. Make sure to structure it like a poem with line breaks using HTML but do not wrap it in backticks , and only write the poem, nothing around it. `;
-  let context = "";
+  let prompt = `Write a short poem about ${topic}. Make sure to structure it like a poem with line breaks using HTML but do not wrap it in backticks , and only write the poem, nothing around it. It should not be longer than 8 lines. `;
+  let context = "You are funny and uplifting.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   axios.get(apiUrl).then(displayPoem);
 }
@@ -10,15 +19,6 @@ function getTopic(event) {
   event.preventDefault();
   let topic = document.querySelector("#topic-input");
   apiCall(topic.value);
-}
-
-function displayPoem(response) {
-  new Typewriter("#poem-field", {
-    strings: response.data.answer,
-    autoStart: true,
-    cursor: "",
-    delay: 1,
-  });
 }
 
 let submitForm = document.querySelector("#submit-form");
